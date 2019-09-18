1bcb708fd07be1faf08f11c9f066df5406489181
DSW1609 <DSW7296666@163.com>
2019-09-18 21:40:06 +0800

    9 18 21 40 DSW

---
+++ js/miaosha.js
@@ -0,0 +1,37 @@
+// 导航栏更多分类触碰弹出隐藏框
+// 获取分类按钮
+var ms_header_more = document.querySelector(".ms_header_more");
+// 获取隐藏框
+var ms_hd_yc = document.querySelector(".ms_hd_yc");
+// 注册触碰事件
+ms_header_more.addEventListener("mouseenter", function() {
+  ms_hd_yc.style.opacity = "1";
+  ms_hd_yc.style.top = "30px";
+  ms_hd_yc.style.display = "block";
+})
+// 注册离开事件
+ms_hd_yc.addEventListener("mouseleave", function() {
+  ms_hd_yc.style.opacity = "0";
+  ms_hd_yc.style.top = "150px";
+  setTimeout(function() {
+    ms_hd_yc.style.display = "none";
+  }, 300)
+})
+// 正在秒杀调用倒计时函数
+TimeDown("#cx_hours", "#cx_minutes", "#cx_seconds", "2019-9-18 24:00:00");
+// 导航栏
+var ms_header_nav = document.querySelector(".ms_header_nav");
+window.addEventListener('scroll', function() {
+  if (pageYOffset >= 115) {
+    ms_header_nav.style.position = "fixed";
+    ms_header_nav.style.top = "0";
+    ms_header_nav.style.borderBottom = "2px solid #e01222";
+    ms_header_nav.style.boxShadow = "1px 1px 10px #666";
+  }
+  else{
+    ms_header_nav.style.position = "absolute";
+    ms_header_nav.style.top = "";
+    ms_header_nav.style.borderBottom = "0";
+    ms_header_nav.style.boxShadow = "";
+  }
+})
